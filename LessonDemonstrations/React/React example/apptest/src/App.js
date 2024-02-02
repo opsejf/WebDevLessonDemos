c3dbfccdd7204885244fea1772718007c27c7477
opsejf <yellowotters1@gmail.com>
2024-02-02 16:22:52 +0000

    adding work done on react test app

--- LessonDemonstrations/React/React example/apptest/src/App.js
+++ LessonDemonstrations/React/React example/apptest/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import Person from './Person.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Person test="superhero" type="UX/UI" colour="pink"/>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
